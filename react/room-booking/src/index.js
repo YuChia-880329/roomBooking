64058ae3c58830981adf50c60a6c39e4895aea5e
許友嘉 <wecomin@avadesign.com.tw>
2023-05-18 18:30:24 +0800

    20230518_Commit_02

--- react/room-booking/src/index.js
+++ react/room-booking/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Test from './test';
-import NavBar from './backend/nav-bar';
+import Navbar from './backend/navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(<Test />);
-root.render(<NavBar />);
+root.render(<Navbar />);
